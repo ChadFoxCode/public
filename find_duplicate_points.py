b1b4624c60679fb23cfcdfad3a55a00e7432631d
Chad Fox <64827175+ChadFoxCode@users.noreply.github.com>
2023-02-07 23:28:01 -0800

    Create find_duplicate_points.py
    Todo: add support for the tolerance argument

---
+++ find_duplicate_points.py
@@ -0,0 +1,98 @@
+
+
+
+def get_duplicate_points(translation_array = None,
+                         rotation_array = None,
+                         tolerance = 0.0,
+                         ):
+
+    #todo impliment tolerance feature. Might be optimal for speed to use rounding decimal values vs numerical comparison?
+
+    t_duplicate_dict = {}
+    r_duplicate_dict = {}
+
+    print(translation_array, rotation_array)
+    # Compare translation values and store indices for values that match 1 or more times.
+    if translation_array:
+        t_compare = {}
+        for i, tup in enumerate(translation_array):
+            if tup in t_compare:
+                if tup not in t_duplicate_dict:
+                    t_duplicate_dict[tup] = [i]
+                else:
+                    t_duplicate_dict[tup].append(i)
+
+                compare_i = t_compare[tup]
+
+                if compare_i not in t_duplicate_dict[tup]:
+                    t_duplicate_dict[tup].append(compare_i)
+            else:
+                t_compare[tup] = i
+        t_duplicate_indices = [i for i in t_duplicate_dict.values()]
+
+    # Compare rotation values and store indices for values that match 1 or more times.
+    if rotation_array:
+        r_compare = {}
+        r_duplicate_dict = {}
+        for i, tup in enumerate(rotation_array):
+            if tup in r_compare:
+                if tup not in r_duplicate_dict:
+                    r_duplicate_dict[tup] = [i]
+                else:
+                    r_duplicate_dict[tup].append(i)
+
+                compare_i = r_compare[tup]
+
+                if compare_i not in r_duplicate_dict[tup]:
+                    r_duplicate_dict[tup].append(compare_i)
+            else:
+                r_compare[tup] = i
+        r_duplicate_indices = [i for i in r_duplicate_dict.values()]
+
+
+    # QC printouts --
+    print(r_duplicate_dict)
+    print(t_duplicate_dict)
+    print(len(r_duplicate_dict), len(t_duplicate_dict))
+
+    if translation_array and not rotation_array:
+        return t_duplicate_indices
+    elif rotation_array and not translation_array:
+        return r_duplicate_indices
+    elif translation_array and rotation_array:
+        duplicate_rt = []
+        for t_index_list in t_duplicate_indices:
+            for r_index_list in r_duplicate_indices:
+                match = set(t_index_list) & set(r_index_list)
+                if len(match) >= 2:
+                    duplicate_rt.append(tuple(match))
+
+
+        print(len(duplicate_rt))
+        print(duplicate_rt)
+
+        return duplicate_rt
+
+# run test when py file executed
+if __name__ == "__main__":
+
+  # example transform array
+  t = [(123.45, 100.1, -9000.1, 42.0),
+       (123.45, 100.1, -9000.1, 42.0),
+       (658.5,-852.852,97.64,31.01),
+       (36.336,-8791.0,658.785,1608.222),
+       (123.45, 100.1, -9000.1, 42.0),
+       (658.5, -852.852, 97.64, 31.01)
+       ]
+  # example rotation array
+  r = [(333.33,222.22,-111.11,777.77777),
+       (333.33,222.22,-111.11,777.77777),
+       (888.5,-777.852,999.64,222.01),
+       (2.336,-2782.0,792.785,44.222),
+       (333.33,222.22,-111.11,777.77777),
+       (888.5,-777.852,999.64,222.01)
+       ]
+
+  get_duplicate_points(t, r)
+  
+#end
